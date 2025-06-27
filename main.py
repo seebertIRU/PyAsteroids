af2ba720f9a812df8460bc5dd38b890367f5f7ff
seebert42 <seebert42@gmail.com>
2025-06-27 05:54:19 -0700

    Circlesprites

--- main.py
+++ main.py
@@ -11,11 +11,15 @@ def main():
 	screen=pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 	screen.fill(pygame.Color(0, 0, 0)) # black
 	pygame.display.flip()
+	clk=pygame.time.Clock()
+	dt=0
 	while True:
 		for event in pygame.event.get():
 			if event.type==pygame.QUIT:
 				return
-
+			elif event.type==pygame.KEYDOWN and event.key==pygame.K_ESCAPE:
+				return
+		dt+=clk.tick(constants.FPS) / 1000	
 
 if __name__ == "__main__":
 	main()
