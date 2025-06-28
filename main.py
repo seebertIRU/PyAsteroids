91d8ae129239b98b949ba2cf857b7cc3b1dc3987
seebert42 <seebert42@gmail.com>
2025-06-28 07:33:23 -0700

    Player1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import constants
-
+import circleshape
+import player
 def main():
 	initresult=pygame.init()
 	if initresult[1] != 0:
@@ -10,10 +11,17 @@ def main():
 	print(f"Screen height: {constants.SCREEN_HEIGHT}")
 	screen=pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 	screen.fill(pygame.Color(0, 0, 0)) # black
+	playerx=constants.SCREEN_WIDTH // 2
+	playery=constants.SCREEN_HEIGHT // 2	
+	plr=player.Player(playerx, playery)
+	print(plr.rotation)
 	pygame.display.flip()
 	clk=pygame.time.Clock()
 	dt=0
 	while True:
+		plr.draw(screen)
+		plr.update(dt)
+		pygame.display.flip()
 		for event in pygame.event.get():
 			if event.type==pygame.QUIT:
 				return
